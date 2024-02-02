e49bfd476bec3a11d5c6515f86958d1e6feb1d12
solomon-winner <solyalew3@gmail.com>
2024-02-02 19:25:49 +0300

    ...

--- Back/index.js
+++ Back/index.js
@@ -10,3 +10,17 @@ app.use(express.json());
 
 // Connect to MongoDB
 
+mongoose.connect('mongodb://mongo:27017/songs_database', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => {
+  console.log('Connected to MongoDB');
+})
+.catch((error) => {
+  console.error('Error connecting to MongoDB:', error);
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
