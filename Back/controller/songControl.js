25cac942aebc384fd0075207dc3e2c52406ff963
solomon-winner <solyalew3@gmail.com>
2024-02-03 20:03:13 +0300

    Save Song ...

--- Back/controller/songControl.js
+++ Back/controller/songControl.js
@@ -1,4 +1,18 @@
-export const saveSong = (req,res) => {}
+export const saveSong = async (req,res) => {
+try {
+    if (!req.body.tittle,
+        !req.body.artist,
+        !req.body.album,
+        !req.body.genre)
+        {
+            return res.status(400).send({
+                message: 'Enter all requires fields!'
+            })
+        }
+} catch (error) {
+
+}
+}
 export const getAll = (req,res) => {}
 export const getOne = (req,res) => {}
 export const updateSong = (req,res) => {}
