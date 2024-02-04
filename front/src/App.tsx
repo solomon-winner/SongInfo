48735a6c169e00503a72e24853f9297dcc1c6192
solomon-winner <solyalew3@gmail.com>
2024-02-04 19:16:48 +0300

    components...

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import './App.css';
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-     
-    </div>
+    <Router>
+      <Routes>
+        <Route path='/' element = {<SongList/>}/>
+        <Route/>
+      </Routes>
+    </Router>
   );
 }
 
