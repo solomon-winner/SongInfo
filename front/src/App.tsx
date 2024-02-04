2fb0a248ada07063bdd444de0a9dee57cd86c778
solomon-winner <solyalew3@gmail.com>
2024-02-04 19:36:01 +0300

    Routing...

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import SongList from './components/SongList';
+import SongDetails from './components/SongDetails';
+import AddForm from './components/AddForm';
+import UpdateForm from './components/UpdateForm';
 
 const App: React.FC = () => {
   return (
     <Router>
       <Routes>
         <Route path='/' element = {<SongList/>}/>
-        <Route/>
+        <Route path = '/details/id' element = {<SongDetails/>}/>
+        <Route path = '/add' element = {<AddForm/>}/>
+        <Route path = '/update/:id' element = {<UpdateForm/>}/>
+        
       </Routes>
     </Router>
   );
