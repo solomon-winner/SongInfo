96026d63827eb4d0150d7fe4920cffd4abba43fe
solomon-winner <solyalew3@gmail.com>
2024-02-04 21:30:10 +0300

    redux SongSlice...

--- front/src/redux/SongSlice.ts
+++ front/src/redux/SongSlice.ts
@@ -9,3 +9,22 @@ interface Song {
 
 const initialState: Song[] = [];
 
+export const SongSlice = createSlice({
+    name: 'songs',
+    initialState,
+    reducers: {
+        setSongs(state, action: PayloadAction<Song[]>){
+            return action.payload;
+        },
+        addSong(state, action: PayloadAction<Song>) {
+            state.push(action.payload);
+        },
+        updateSong(state, action: PayloadAction<Song>) {
+
+                },
+        deleteSong(state, action: PayloadAction<Song>) {
+
+        }
+    }
+})
+ export const {setSongs, addSong, updateSong, deleteSong} = SongSlice.actions
