4683f2fa465993ae68a27c937b8a30ede561fe09
solomon-winner <solyalew3@gmail.com>
2024-02-04 19:19:56 +0300

    The list...

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
-import './App.css';
+import SongList from './components/SongList';
 
 const App: React.FC = () => {
   return (
