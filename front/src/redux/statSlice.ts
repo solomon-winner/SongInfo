3097b73021fcfdb5a28cb14b6331b746da86a2d4
solomon-winner <solyalew3@gmail.com>
2024-02-06 11:48:43 +0300

    the statistics...

---
+++ front/src/redux/statSlice.ts
@@ -0,0 +1,14 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const statSlice = createSlice({
+    name: 'stat',
+    initialState: {
+        totalSong: 0,
+        artists: 0,
+        albums: 0,
+        genres: 0,
+    },
+    reducers : {
+        
+    }
+}) 
