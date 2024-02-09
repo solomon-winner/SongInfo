c0a19e3a00b3bc7e0f75eaff2100b98321c5ddc6
solomon-winner <solyalew3@gmail.com>
2024-02-09 22:18:28 +0300

    ...

--- front/src/page/Home.tsx
+++ front/src/page/Home.tsx
@@ -1,12 +1,16 @@
 
 import React from "react";
 import  Nav  from "../components/Nav";
+import MainSect from "../components/Main";
+import FooterSect from "../components/Footer";
 
 const Home:React.FC = () => {
     
     return (
         <div >
             <Nav/>
+            <MainSect/>
+            <FooterSect/>
         </div>
     )
     
