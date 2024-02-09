2a146fff6cca90f129578e980c76ea88057de551
solomon-winner <solyalew3@gmail.com>
2024-02-09 22:12:11 +0300

    ...

--- front/src/components/Nav.tsx
+++ front/src/components/Nav.tsx
@@ -14,38 +14,6 @@ const App = () => {
                 </NavLinks>
             </Nav>
 
-            <Main>
-                <Upper>
-                    <Moto>
-                        <h1>Feel <strong>Every Beat</strong></h1>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur, facere ut possimus voluptas eum eos distinctio optio nulla velit debitis quasi doloremque nihil odio error! Quis recusandae eius deleniti at!</p>
-                        <Button>Get Started</Button>
-                    </Moto>
-                </Upper>
-
-                <Lower>
-                    <Wrapper>
-                        <Text>
-                            <Title>Song Information</Title>
-                            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptates et dicta voluptas, voluptatum quibusdam, non odit laborum veritatis odio beatae obcaecati nihil asperiores, exercitationem officia vitae velit dolorum! Delectus, debitis!</p>
-                        </Text>
-
-                        <Text>
-                            <Title>Song Information</Title>
-                            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptates et dicta voluptas, voluptatum quibusdam, non odit laborum veritatis odio beatae obcaecati nihil asperiores, exercitationem officia vitae velit dolorum! Delectus, debitis!</p>
-                        </Text>
-
-                        <Text>
-                            <Title>Song Information</Title>
-                            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptates et dicta voluptas, voluptatum quibusdam, non odit laborum veritatis odio beatae obcaecati nihil asperiores, exercitationem officia vitae velit dolorum! Delectus, debitis!</p>
-                        </Text>
-                    </Wrapper>
-                </Lower>
-            </Main>
-
-            <Footer>
-                <p>&copy; Copyright 2024 solomon yalew</p>
-            </Footer>
         </>
     );
 };
