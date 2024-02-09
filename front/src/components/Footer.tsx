9f285d84adb336eecf860316d13be8c516c2d244
solomon-winner <solyalew3@gmail.com>
2024-02-09 22:14:34 +0300

    ...

--- front/src/components/Footer.tsx
+++ front/src/components/Footer.tsx
@@ -1,6 +1,10 @@
 import React from 'react'
-
-const Footer:React.FC = () => {
-    return ()
+import {Footer} from "../style/nav"
+const FooterSect:React.FC = () => {
+    return (
+        <Footer>
+                <p>&copy; Copyright 2024 solomon yalew</p>
+            </Footer>
+    )
 }
-export default Footer;
+export default FooterSect;
