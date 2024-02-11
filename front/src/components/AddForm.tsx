c12c6411427c28b5ed232b2ef87322013ea0963a
solomon-winner <solyalew3@gmail.com>
2024-02-11 20:18:35 +0300

    ...

--- front/src/components/AddForm.tsx
+++ front/src/components/AddForm.tsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import { AddSong, Button, Buttons, Form, Input, Label, Row, Title } from '../style/Add';
 
 const AddForm:React.FC = () => {
     return (
         <>
-        
+        <AddSong>
+            <Title> Add New Song</Title>
+            <Form>
+                <Row>
+                    <Label>Artist: </Label> <Input/>
+                    <Label>Album: </Label> <Input/>
+                    <Label>Genre: </Label> <Input/>
+                    <Label>Title: </Label> <Input/>
+
+                    <Buttons>
+                        <Button>Add To The List</Button>
+                    </Buttons>
+                </Row>
+            </Form>
+        </AddSong>
         </>
     )
 }
