707fe3ad4e2232d5c7f627c5e58d208fa5af0ce2
solomon-winner <solyalew3@gmail.com>
2024-02-11 11:13:37 +0300

    ...

--- front/src/components/StatDisplay.tsx
+++ front/src/components/StatDisplay.tsx
@@ -1,21 +1,41 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Stat, Info, Number, Para } from '../style/Stat';
 import { StatType } from '../Store/Types';
-import { useDispatch, useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
 const StatDisplay: React.FC = () => {
 
-    const dispatcher = useDispatch();
+    const [albumsCount, setAlbumsCount] = useState(0);
+    const [artistsCount, setArtistsCount] = useState(0);
+    const [songsCount, setSongsCount] = useState(0);
+    const [genresCount, setGenresCount] = useState(0);
+
+
     const Albums =useSelector((state:StatType) => state.stat.albums);
     const Artists =useSelector((state:StatType) => state.stat.artists);
     const Songs =useSelector((state:StatType) => state.stat.songs);
     const Genres =useSelector((state:StatType) => state.stat.genres);
 
+    useEffect(() => {
+        countUp(Albums, setAlbumsCount);
+        countUp(Artists, setArtistsCount);
+        countUp(Songs, setSongsCount);
+        countUp(Genres, setGenresCount);
+    }, [Albums, Artists, Songs, Genres])
+
+    const countUp = (target:number, setter: React.Dispatch<React.SetStateAction<number>>) => {
+        let current = 0;
+        const interval = setInterval(() => {
+            current++;
+            setter(current);
+            if (current === target) clearInterval(interval);
+        }, 50);
+    }
     return (
         <Stat className="stat">
-        <Info><Number>0 </Number><Para>Albums</Para></Info>
-        <Info><Number>0</Number><Para>Artists</Para></Info>
-        <Info><Number>0</Number><Para>Songs</Para></Info>
-        <Info><Number>0</Number><Para>Genres</Para></Info>
+        <Info><Number>{albumsCount} </Number><Para>Albums</Para></Info>
+        <Info><Number>{artistsCount}</Number><Para>Artists</Para></Info>
+        <Info><Number>{songsCount}</Number><Para>Songs</Para></Info>
+        <Info><Number>{genresCount}</Number><Para>Genres</Para></Info>
 
     </Stat>
     )
