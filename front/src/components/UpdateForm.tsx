248b534717d8416500ed46317e39540ad29caa91
solomon-winner <solyalew3@gmail.com>
2024-02-11 20:55:26 +0300

    ...

--- front/src/components/UpdateForm.tsx
+++ front/src/components/UpdateForm.tsx
@@ -1,8 +1,34 @@
-import React from'react'
+import React from 'react'
+import { AddSong, Buttons, Cross, Form, Input, Label, Row, Title } from '../style/Add';
+import { useDispatch } from 'react-redux';
+import { showAdd } from '../Store/DisplaySlice';
+import { Edit } from '../style/detail';
 
 const UpdateForm:React.FC = () => {
+const dispatch = useDispatch()
+const DontShowAdd = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    e.preventDefault();
+    dispatch(showAdd(false))
+}
+  
     return (
-        <div className=""></div>
+        <>
+        <AddSong>
+        <Title> Add New Song</Title>
+            <Form>
+                    <Row><Label>Artist: </Label><Input/></Row>    
+                    <Row><Label>Album: </Label><Input/></Row>
+                    <Row><Label>Genre: </Label><Input/></Row>
+                    <Row><Label>Title: </Label><Input/></Row>
+
+                    <Buttons>
+                        <Edit>Update the Song</Edit>
+                    </Buttons>
+                
+            </Form>
+            <Cross onClick={DontShowAdd}><img src="../assets/xmark-solid.svg" alt="" /></Cross>
+        </AddSong>
+        </>
     )
 }
 export default UpdateForm;
