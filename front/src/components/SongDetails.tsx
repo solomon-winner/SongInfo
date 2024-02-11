926abf6ef7f7ea530a6eb30d78cbc1f577347a57
solomon-winner <solyalew3@gmail.com>
2024-02-11 17:28:57 +0300

    ...

--- front/src/components/SongDetails.tsx
+++ front/src/components/SongDetails.tsx
@@ -1,10 +1,30 @@
 import React from 'react';
-
+import { Another, Buttons, Cross, Delete, Detail, Edit, Label, Row, SongDetail, Value, } from '../style/detail';
 const SongDetails:React.FC = () => {
     return(
-        <div className="">
-        <h1>Song Details</h1>
-        </div>
+       <>
+       <Detail>
+        <SongDetail> Song Detail</SongDetail>
+        <Row> <Label>Artist: </Label> 
+        <Value>Aster Abebe</Value>
+        </Row>
+        <Row> <Label>Album: </Label> 
+        <Value>Aster Abebe</Value>
+        </Row>
+        <Row> <Label>Genre: </Label> 
+        <Value>Aster Abebe</Value>
+        </Row>
+        <Row> <Label>Title: </Label> 
+        <Value>Aster Abebe</Value>
+        </Row>
+        <Buttons>
+            <Edit>Edit</Edit>
+            <Another>Songs By This Artist</Another>
+            <Delete>Delete</Delete>
+        </Buttons>
+        <Cross></Cross>
+       </Detail>
+       </>
     )
 }
 
