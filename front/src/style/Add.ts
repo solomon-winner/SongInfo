6eabd9f48d03db1182fe1760218cf37e20ce64ef
solomon-winner <solyalew3@gmail.com>
2024-02-11 20:07:23 +0300

    ...

---
+++ front/src/style/Add.ts
@@ -0,0 +1,10 @@
+import styled from "@emotion/styled";
+
+export const AddSong = styled.div``
+export const Title = styled.div``
+export const Form = styled.form``
+export const Row =styled.div``
+export const Label = styled.span``
+export const Input = styled.input``
+export const Buttons = styled.div``
+export const Button = styled.button``
