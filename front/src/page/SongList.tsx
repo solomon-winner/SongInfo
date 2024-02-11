a403f708122c9cce3ad69ccec60de4c6f6e9a79d
solomon-winner <solyalew3@gmail.com>
2024-02-11 11:52:15 +0300

    ...

--- front/src/page/SongList.tsx
+++ front/src/page/SongList.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Nav  from '../components/Nav';
 import { Wrapper } from '../style/List';
+import  Stat  from '../components/StatDisplay';
 const  SongList: React.FC = () => {
 return (
     <>
     <Nav/>
     <Wrapper/>
+    <Stat/>
     </>
 )
 }
