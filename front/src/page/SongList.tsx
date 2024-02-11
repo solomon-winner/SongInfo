e6a7a3e4500d5e1d19ab4343215d1e2c72845733
solomon-winner <solyalew3@gmail.com>
2024-02-11 19:07:32 +0300

    ...

--- front/src/page/SongList.tsx
+++ front/src/page/SongList.tsx
@@ -4,16 +4,25 @@ import StatDisplay from '../components/StatDisplay';
 import { Wrapper, Main, Search, Rest, Song, Img, Desc, Title, ArtistName, Form, Input, Button, Add, Container} from '../style/List';
 import FooterSect from '../components/Footer';
 import SongDetails from '../components/SongDetails';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { DisplayType } from '../Store/Types';
+import { setPopUp } from '../Store/DisplaySlice';
+
 const  SongList: React.FC = () => {
 
 const popUp = useSelector((state: DisplayType) => state.display.popUp)
+console.log("this is the popUp state: " + popUp);
+const dispatch = useDispatch();
+
+const showDetail = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    e.preventDefault();
+    dispatch(setPopUp(true))
+}
 return (
     <>
     <Nav/>
     <Wrapper/>
-    <SongDetails/>        
+   {popUp && <SongDetails/> }       
 
     <Main>
            <Container><StatDisplay/><Add> Add Song</Add> </Container> 
@@ -27,7 +36,7 @@ return (
                 </Form>
             </Search>
             <Rest>
-                <Song>
+                <Song onClick={showDetail}>
                     <Img src='../assets/headphones-3085681_1280.jpg'/>
                     <Desc>
                         <Title>Kedmom Kibir</Title>
