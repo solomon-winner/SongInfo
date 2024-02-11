286b4c9e638141212a2724647075a5abf1062c2a
solomon-winner <solyalew3@gmail.com>
2024-02-11 17:04:35 +0300

    ...

---
+++ front/src/style/detail.ts
@@ -0,0 +1,9 @@
+import styled from "@emotion/styled";
+
+export const Detail = styled.div``
+export const SongDetail = styled.div``
+export const Row = styled.div``
+export const Label = styled.span``
+export const Value = styled.span``
+export const Buttons = styled.div``
+export const Button = styled.button``
