72f448a80ee6ff6d97f97e2a8547094cc0c0286b
solomon-winner <solyalew3@gmail.com>
2024-02-11 11:21:41 +0300

    Style the List ...

--- front/src/page/SongList.tsx
+++ front/src/page/SongList.tsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import Nav  from '../components/Nav';
 const  SongList: React.FC = () => {
 return (
-    <div className="">
-    </div>
+    <>
+    <Nav/>
+    
+    </>
 )
 }
 export default SongList;
