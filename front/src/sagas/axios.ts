f995a2dbd965c255d2c67b53f151591ca358b53c
solomon-winner <solyalew3@gmail.com>
2024-02-13 11:32:08 +0300

    Axios...

--- front/src/sagas/axios.ts
+++ front/src/sagas/axios.ts
@@ -3,5 +3,17 @@ import axios from "axios";
 const BaseURL = "http://localhost:8080"
 
 export const Fetch = async() => {
-    
+    try {} catch (error) {}
+}
+export const Add = async() => {
+    try {} catch (error) {}
+}
+export const Update = async() => {
+    try {} catch (error) {}
+}
+export const Delete = async() => {
+    try {} catch (error) {}
+}
+export const Stat = async() => {
+    try {} catch (error) {}
 }
