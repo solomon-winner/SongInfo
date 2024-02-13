72eb8a4aed558992454f589d5a8b089a4b577d80
solomon-winner <solyalew3@gmail.com>
2024-02-13 10:34:26 +0300

    ...

--- Back/routes/SongRoute.js
+++ Back/routes/SongRoute.js
@@ -4,9 +4,9 @@ import { saveSong, getAll, getStat, updateSong, deleteSong } from '../controller
 const router = express.Router();
 
 router.post("/",saveSong);
-//router.get("/",getAll);
+router.get("/",getAll);
 router.put("/:id",updateSong);
 router.delete("/:id",deleteSong);
 router.get("/",getStat);
 
-export default router
+export default router;
