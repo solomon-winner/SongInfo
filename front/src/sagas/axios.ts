bc2835cfe8b314da2ca5eacf266b4b4842618f21
solomon-winner <solyalew3@gmail.com>
2024-02-13 12:42:13 +0300

    axios error...

--- front/src/sagas/axios.ts
+++ front/src/sagas/axios.ts
@@ -1,9 +1,23 @@
-import axios from "axios";
+import axios, { AxiosError } from "axios";
 
 const BaseURL = "http://localhost:8080"
 
-export const Fetch = async() => {
-    try {} catch (error) {}
+interface Data {
+    tittle: string,
+    artist: string,
+    album: string;
+
+}
+const handleError = (error: AxiosError, defaultMessage: string ) : void => {
+
+}
+export const Fetch = async(): Promise<Data[]> => {
+    try {
+        const response = await axios.get(`${BaseURL}/Back/Song`);
+        return response.data;
+    } catch (error) {
+        // throw new Error(error.response.data.message)
+    }
 }
 export const Add = async() => {
     try {} catch (error) {}
