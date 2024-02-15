271089bfa331a9bf0a26ae5730d80a812b18fc69
solomon-winner <solyalew3@gmail.com>
2024-02-15 22:34:55 +0300

    ...

---
+++ front/src/Store/SelectedSongSlice.ts
@@ -0,0 +1,19 @@
+import { PayloadAction, createSlice } from "@reduxjs/toolkit";
+import { Song } from "./SongSlice";
+
+const initialState: Song= {
+    _id: '',
+    tittle: '',
+    artist: '',
+    album: '',
+    genre: ''
+}
+const selectedSlice = createSlice({
+    name: 'selected',
+    initialState,
+    reducers: {
+        setSelected (state, action: PayloadAction<Song>) {
+
+        }
+    }
+})
