0d551c0fd863b5a8ac19027c9f0819a9932be1df
solomon-winner <solyalew3@gmail.com>
2024-02-15 19:47:16 +0300

    ...

--- front/src/components/SongDetails.tsx
+++ front/src/components/SongDetails.tsx
@@ -1,12 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Another, Buttons, Cross, Delete, Detail, Edit, Label, Row, SongDetail, Value, } from '../style/detail';
 import { useDispatch, useSelector } from 'react-redux';
 import { showDelete, showDetail, showUpdate } from '../Store/DisplaySlice';
 import { useParams } from 'react-router-dom';
 import { FetchType } from '../Store/Types';
 import {Song} from "../Store/SongSlice"
+import {songLoading} from "../Store/SongSlice"
 const SongDetails:React.FC = () => {
 const dispatch = useDispatch()
+
+useEffect(() => {
+    dispatch(songLoading())
+}, [dispatch])
 
 const {_id} = useParams<{_id:string}>()
 const detail = useSelector((state:FetchType) => state.songs.songs.find((song:Song) => song._id === _id));
@@ -31,16 +36,16 @@ const ShowDelete = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) =>{
         <SongDetail> Song Detail</SongDetail>
        {detail && <>
         <Row> <Label>Artist: </Label> 
-        <Value>Aster Abebe</Value>
+        <Value>{detail.artist}</Value>
         </Row>
         <Row> <Label>Album: </Label> 
-        <Value>Aster Abebe</Value>
+        <Value>{detail.album}</Value>
         </Row>
         <Row> <Label>Genre: </Label> 
-        <Value>Aster Abebe</Value>
+        <Value>{detail.genre}</Value>
         </Row>
         <Row> <Label>Title: </Label> 
-        <Value>Aster Abebe</Value>
+        <Value>{detail.tittle}</Value>
         </Row>
         </>
         }
