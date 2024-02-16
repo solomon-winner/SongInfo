7e97f8baa3c6ebd7cd7397d69aaed9bd0b2937a2
solomon-winner <solyalew3@gmail.com>
2024-02-17 01:40:26 +0300

    ...

--- front/src/components/UpdateForm.tsx
+++ front/src/components/UpdateForm.tsx
@@ -23,7 +23,7 @@ const DontShowUpdate = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
     dispatch(showUpdate(false))
 }
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>)=> {
-    e.preventDefault()
+    e.preventDefault();
   }
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -33,10 +33,10 @@ const DontShowUpdate = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
         <AddSong>
         <Title> Add New Song</Title>
             <Form>
-                    <Row><Label>Artist: </Label><Input onChange={handleChange} value={selected?.artist}/></Row>    
-                    <Row><Label>Album: </Label><Input onChange={handleChange} value={selected?.album}/></Row>
-                    <Row><Label>Genre: </Label><Input onChange={handleChange} value={selected?.genre}/></Row>
-                    <Row><Label>Title: </Label><Input onChange={handleChange} value={selected?.tittle}/></Row>
+                    <Row><Label>Artist: </Label><Input name = 'artist' onChange={handleChange} value={selected?.artist}/></Row>    
+                    <Row><Label>Album: </Label><Input name = 'album' onChange={handleChange} value={selected?.album}/></Row>
+                    <Row><Label>Genre: </Label><Input name = 'genre' onChange={handleChange} value={selected?.genre}/></Row>
+                    <Row><Label>Title: </Label><Input name = 'tittle' onChange={handleChange} value={selected?.tittle}/></Row>
 
                     <Buttons>
                         <Edit>Update the Song</Edit>
