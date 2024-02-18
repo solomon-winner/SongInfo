e7f654822243814a7c8748a48d9d908f7c31e752
solomon-winner <solyalew3@gmail.com>
2024-02-18 17:01:21 +0300

    ...

--- front/src/components/Another.tsx
+++ front/src/components/Another.tsx
@@ -3,7 +3,67 @@ import { Wrapper, Another, Top, Cross, Name, Stat, Count, Number, Span, Songs, S
 const AnotherSong:React.FC = () => {
     return (
         <Wrapper>
-            
+            <Another>
+                <Top>
+                    <Name>
+                        <h1>Tesfaye Gabiso</h1>
+                    </Name>
+                    <Cross>
+                        <img src="" alt="" />
+                    </Cross>
+                    </Top>
+                    <Stat>
+                        <Count><Number>68</Number><Span>Songs</Span></Count>
+                        <Count><Number>30</Number><Span>Album</Span></Count>
+                        <Count><Number>23</Number><Span>Genre</Span></Count>
+
+                    </Stat>
+
+                    <Songs>
+                        <Song>
+                            <Left>
+                                <Icon></Icon>
+                                Yeadebabay sew Honeh
+                            </Left>
+                            <Button> Detail Of The Song</Button>
+                        </Song>
+
+                        <Song>
+                            <Left>
+                                <Icon></Icon>
+                                Yeadebabay sew Honeh
+                            </Left>
+                            <Button> Detail Of The Song</Button>
+                        </Song>
+
+                        <Song>
+                            <Left>
+                                <Icon></Icon>
+                                Yeadebabay sew Honeh
+                            </Left>
+                            <Button> Detail Of The Song</Button>
+                        </Song>
+
+                        <Song>
+                            <Left>
+                                <Icon></Icon>
+                                Yeadebabay sew Honeh
+                            </Left>
+                            <Button> Detail Of The Song</Button>
+                        </Song>
+
+                        <Song>
+                            <Left>
+                                <Icon></Icon>
+                                Yeadebabay sew Honeh
+                            </Left>
+                            <Button> Detail Of The Song</Button>
+                        </Song>
+                        
+                    </Songs>
+
+
+            </Another>
         </Wrapper>
     )
 }
