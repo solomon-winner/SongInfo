a649fc0a190f02e7471815371cfc15f809abc2a2
solomon-winner <solyalew3@gmail.com>
2024-02-18 16:11:44 +0300

    ...

---
+++ front/src/style/Another.ts
@@ -0,0 +1,10 @@
+import styled from '@emotion/styled'
+
+export const Wrapper = styled.div`
+width: 100%;
+height: 100vh;
+display: flex;
+align-items: center;
+justify-content: center;
+background-color: black;
+`
