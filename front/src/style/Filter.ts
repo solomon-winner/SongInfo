5e9b27a5c4e126a89fccb4d20c7d00babcc893b4
solomon-winner <solyalew3@gmail.com>
2024-02-19 16:56:45 +0300

    ...

--- front/src/style/Filter.ts
+++ front/src/style/Filter.ts
@@ -1,2 +1,30 @@
 import styled from "@emotion/styled";
 
+
+export const Menu = styled.div`
+width: 100%;
+z-index: 100000;
+display: flex;
+justify-content: center;
+`
+export const Choose = styled.div`
+width: 50%;
+z-index: 100000;
+display: flex;
+flex-direction: column;
+gap:0.5rem;
+background-color: rgb(1, 4, 32);
+
+`
+export const Choice = styled.div`
+color: rgb(255, 115, 0);
+font-size: medium;
+font-family: Georgia, 'Times New Roman', Times, serif;
+padding: 0.7rem;
+cursor: pointer;
+
+&:hover {
+    background-color: rgb(62, 64, 66);
+
+}
+`
