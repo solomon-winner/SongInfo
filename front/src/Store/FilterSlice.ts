4f26edd7f322d9d49ebf1b98d8c971585463c6da
solomon-winner <solyalew3@gmail.com>
2024-02-19 16:16:48 +0300

    ...

--- front/src/Store/FilterSlice.ts
+++ front/src/Store/FilterSlice.ts
@@ -13,3 +13,25 @@ const initialState: FilterBy = {
     artist: false,
     genre: false
 }
+
+const FilterSlice = createSlice ({
+    name: 'filter',
+    initialState,
+    reducers: {
+        titleInput (state, action:PayloadAction<boolean>) {
+
+        },
+        albumInput (state, action:PayloadAction<boolean>) {
+            
+        },
+        artistInput (state, action:PayloadAction<boolean>) {
+            
+        },
+        genreInput (state, action:PayloadAction<boolean>) {
+            
+        }
+    }
+})
+
+export const {titleInput, albumInput, artistInput, genreInput} = FilterSlice.actions;
+export default FilterSlice;
