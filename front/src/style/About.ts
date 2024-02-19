915f7769691e2aa3f54a14ab3df0cecfec234ac9
solomon-winner <solyalew3@gmail.com>
2024-02-20 01:44:21 +0300

    ...

--- front/src/style/About.ts
+++ front/src/style/About.ts
@@ -1,1 +1,13 @@
 import styled from '@emotion/styled'
+import { Link } from 'react-router-dom'
+
+export const Wrapper = styled.div``
+export const H = styled.h1``
+export const MyPhoto = styled.img``
+export const Description = styled.div``
+export const Name = styled.div``
+export const Contacts = styled.div``
+export const Phone = styled.div``
+export const LinkedIn = styled.div``
+export const Github = styled.div``
+export const Linker = styled(Link)``
