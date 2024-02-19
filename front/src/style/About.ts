f936a90763e1eed899a7921a795257f728ac04a8
solomon-winner <solyalew3@gmail.com>
2024-02-20 01:46:57 +0300

    ...

--- front/src/style/About.ts
+++ front/src/style/About.ts
@@ -1,9 +1,27 @@
 import styled from '@emotion/styled'
 import { Link } from 'react-router-dom'
 
-export const Wrapper = styled.div``
-export const H = styled.h1``
-export const MyPhoto = styled.img``
+export const Wrapper = styled.div`
+width: 100%;
+height: 100vh;
+display: flex;
+align-items: center;
+justify-content: center;
+background-color: rgb(0, 0, 0);
+`
+export const H = styled.h1`
+color: white;
+display: flex;
+padding: 15px;
+justify-content: center;
+`
+export const MyPhoto = styled.img`
+width: 250px;
+height: 250px;
+object-fit: cover;
+border-radius: 50%;
+border: 2px solid rgb(255, 115, 0);
+`
 export const Description = styled.div``
 export const Name = styled.div``
 export const Contacts = styled.div``
