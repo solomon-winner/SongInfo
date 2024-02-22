a193872e8fb9d9982875fcdeea07a34e6ef8d304
solomon-winner <solyalew3@gmail.com>
2024-02-22 10:24:09 +0300

    ...

---
+++ front/src/style/error.ts
@@ -0,0 +1,30 @@
+import styled from '@emotion/styled'
+
+export const Cross =styled.span`
+color: white;
+font-size: medium;
+font-weight: bold;
+font-family: Georgia, 'Times New Roman', Times, serif;
+display: flex;
+justify-content: end;
+
+`
+export const Message = styled.div`
+ 
+    color: white;
+    font-size: medium;
+    font-weight: bold;
+    font-family: Georgia, 'Times New Roman', Times, serif;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 50%;
+
+`
+export const Error = styled.div`
+width: 30%;
+height: 20vh;
+border: 2px solid rgb(255, 115, 0);
+background-color: black;
+}
+`
